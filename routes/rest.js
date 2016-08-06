37d1a0d6f4297eba1c7c181ac7e2f127a77ee3bd
thang <thang.buinguyen@yahoo.com.vn>
2016-08-06 20:20:40 +0800

    temporary disable db

--- routes/rest.js
+++ routes/rest.js
@@ -2,7 +2,7 @@ var express = require('express');
 var _ = require('lodash');
 var router = express.Router();
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/orders');
+// mongoose.connect('mongodb://localhost/orders');
 mongoose.Promise = global.Promise;
 
 var Order = mongoose.model('Order',
