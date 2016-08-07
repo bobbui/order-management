5171ac575d461f75b8a8a3962445a0d0a23857f2
thang <thang.buinguyen@yahoo.com.vn>
2016-08-08 00:05:45 +0800

    polish UI

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,1 +1,10 @@
-'use strict'
+(function (document) {
+    'use strict';
+
+    app.data = {page: "orders"};
+    app._onPageSelected = function () {
+        console.log("_onPageSelected");
+        console.log(pages.selected);
+        window[pages.selected].init();
+    }
+})(document);
--- routes/rest.js
+++ routes/rest.js
@@ -2,14 +2,15 @@ var express = require('express');
 var _ = require('lodash');
 var router = express.Router();
 var mongoose = require('mongoose');
-// mongoose.connect('mongodb://localhost/orders');
+mongoose.connect('mongodb://localhost/orders');
 mongoose.Promise = global.Promise;
 
 var Order = mongoose.model('Order',
     {
+        name: String,
         type: String,
         size: String,
-        unitPrice: Number,
+        price: Number,
         quantity: Number,
         amount: Number,
         time: Date
@@ -20,6 +21,7 @@ var Drink = mongoose.model('Drink',
         size: String,
         price: Number,
     });
+
 router.get('/drink', function (req, res, next) {
     Drink.find(function (err, drinks) {
         if (err) {
@@ -31,17 +33,6 @@ router.get('/drink', function (req, res, next) {
     });
 });
 
-router.put('/order', function (req, res, next) {
-    var newOrder = new Order(req.body);
-    newOrder.time = new Date();
-    newOrder.save(function (err) {
-        // if (err) {
-        res.status(500).send({success: false, message: err});
-        /* } else {
-         res.send({success: true});
-         }*/
-    });
-});
 router.get('/order', function (req, res, next) {
     var groupByParam = req.query["groupBy"];
     if (_.isEmpty(groupByParam)) {
@@ -71,4 +62,15 @@ router.get('/order', function (req, res, next) {
     }
 });
 
+router.put('/order', function (req, res, next) {
+    var newOrder = new Order(req.body);
+    newOrder.time = new Date();
+    newOrder.save(function (err) {
+        if (err) {
+            res.status(500).send({success: false, message: err});
+        } else {
+            res.send({success: true});
+        }
+    });
+});
 module.exports = router;
